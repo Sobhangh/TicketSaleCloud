2b2cafc989ae2669b95bd2ebdab6834f6eb8d736
Sobhangh <47889077+Sobhangh@users.noreply.github.com>
2024-06-25 16:33:11 +0200

    Update Model.java

--- src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/Model.java
@@ -1,6 +1,12 @@
 package be.kuleuven.distributedsystems.cloud;
 
 import be.kuleuven.distributedsystems.cloud.entities.*;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.ByteArray;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.TopicName;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.ParameterizedTypeReference;
@@ -9,6 +15,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.annotation.Resource;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.time.LocalDateTime;
 import java.util.*;
 
@@ -17,10 +26,9 @@ public class Model {
     @Resource(name = "webClientBuilder")
     WebClient.Builder webClientBuilder;
 
-
-
+    // This way the bookings will be available only in one session?????
+    List<Booking> bookings = new ArrayList<>();
     public List<Show> getShows() {
-
         List<Show> shows = new ArrayList(webClientBuilder.baseUrl("https://reliabletheatrecompany.com/" ).build()
                 .get().uri(uriBuilder -> uriBuilder.pathSegment("shows")
                         .queryParam("key","wCIoTqec6vGJijW2meeqSokanZuqOL").build())
@@ -33,39 +41,83 @@ public class Model {
 
     public Show getShow(String company, UUID showId) {
         // TODO: return the given show
-        return null;
+        Show show = webClientBuilder.baseUrl("https://"+company).build().get().uri(uriBuilder ->
+                uriBuilder.pathSegment("/shows/"+showId)
+                        .queryParam("key","wCIoTqec6vGJijW2meeqSokanZuqOL").build()).retrieve()
+                .bodyToMono(Show.class).block();
+        return show;
     }
 
 
 
     public List<LocalDateTime> getShowTimes(String company, UUID showId) {
         // TODO: return a list with all possible times for the given show
-        return new ArrayList<>();
+        List<LocalDateTime> l = new ArrayList(webClientBuilder.baseUrl("https://"+company).build().get().uri(uriBuilder ->
+                uriBuilder.pathSegment("/shows/"+showId+"/times")
+                        .queryParam("key","wCIoTqec6vGJijW2meeqSokanZuqOL").build()).retrieve()
+                .bodyToMono(new ParameterizedTypeReference<CollectionModel<LocalDateTime>>() {
+                }).block().getContent());
+        return l;
     }
 
     public List<Seat> getAvailableSeats(String company, UUID showId, LocalDateTime time) {
         // TODO: return all available seats for a given show and time
-        return new ArrayList<>();
+        List<Seat> l = new ArrayList(webClientBuilder.baseUrl("https://"+company).build().get().uri(uriBuilder ->
+                uriBuilder.pathSegment("/shows/"+showId+"/seats")
+                        .queryParam("key","wCIoTqec6vGJijW2meeqSokanZuqOL").queryParam("time",time)
+                        .queryParam("available",true).build()).retrieve()
+                .bodyToMono(new ParameterizedTypeReference<CollectionModel<Seat>>() {
+                }).block().getContent());
+        return l;
     }
-
+    //https://reliabletheatrecompany.com/shows/d843d1cb-bbee-4423-b444-eed26a7d5d12/seats?time=2021-12-20T15:00:00&available=true&key=wCIoTqec6vGJijW2meeqSokanZuqOL
+    //url for getting seat of mamamia at 20 december 15:00
     public Seat getSeat(String company, UUID showId, UUID seatId) {
-        // TODO: return the given seat
+        try{
+            Seat s = webClientBuilder.baseUrl("https://"+company).build().get().uri(uriBuilder ->
+                    uriBuilder.pathSegment("shows").pathSegment(showId.toString())
+                            .pathSegment("seats").pathSegment(seatId.toString())
+                            .queryParam("key","wCIoTqec6vGJijW2meeqSokanZuqOL").build()).retrieve()
+                    .bodyToMono(Seat.class).block();
+            return s;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     public Ticket getTicket(String company, UUID showId, UUID seatId) {
         // TODO: return the ticket for the given seat
+
+        try{
+            Ticket t = webClientBuilder.baseUrl("https://" + company).build().get().uri(uriBuilder ->
+                    uriBuilder.pathSegment("/shows"  ).pathSegment(showId.toString())
+                            .pathSegment("seats").pathSegment(seatId.toString())
+                            .pathSegment("ticket")
+                            .queryParam("key", "wCIoTqec6vGJijW2meeqSokanZuqOL")
+                            .build()).retrieve()
+                    .bodyToMono(Ticket.class).block();
+            return t;
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     public List<Booking> getBookings(String customer) {
         // TODO: return all bookings from the customer
-        return new ArrayList<>();
+        List<Booking> b = new ArrayList<>();
+        for(Booking booking:bookings){
+            if(booking.getCustomer()==customer){
+                b.add(booking);
+            }
+        }
+        return b;
     }
 
     public List<Booking> getAllBookings() {
         // TODO: return all bookings
-        return new ArrayList<>();
+        return bookings;
     }
 
     public Set<String> getBestCustomers() {
@@ -73,7 +125,62 @@ public class Model {
         return null;
     }
 
-    public void confirmQuotes(List<Quote> quotes, String customer) {
+    public void confirmQuotes(List<Quote> quotes, String customer) throws IOException {
         // TODO: reserve all seats for the given quotes
+        List<Ticket> tickets = new ArrayList<>();
+        List<String> l = new ArrayList<>();
+        l.add(customer);
+        try {
+            for (Quote q : quotes) {
+                // have to do the all or none semantic....
+                /**
+                Ticket t =webClientBuilder.baseUrl("https://" + q.getCompany()).build().put().uri(uriBuilder ->
+                        uriBuilder.pathSegment("shows").pathSegment(q.getShowId().toString())
+                                .pathSegment("seats").pathSegment(q.getSeatId().toString())
+                                .pathSegment("ticket")
+                                .queryParam("key", "wCIoTqec6vGJijW2meeqSokanZuqOL")
+                                .queryParam("customer", customer).build()).retrieve()
+                        .bodyToMono(Ticket.class).block();
+                tickets.add(t);
+                System.out.println(t.getCompany());**/
+                l.add("");
+                l.add(q.getCompany());
+                l.add(q.getShowId().toString());
+                l.add(q.getSeatId().toString());
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        TopicName topicName = TopicName.of("demo-distributed-systems-kul", "Booking");
+
+        Publisher publisher = null;
+        try {
+            // Create a publisher instance with default settings bound to the topic
+            publisher = Publisher.newBuilder(topicName).build();
+
+            final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            final ObjectOutputStream objectOutputStream =
+                    new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(l);
+            objectOutputStream.flush();
+            objectOutputStream.close();
+            final byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+            PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(ByteString.copyFrom(byteArray)).build();
+
+            // Once published, returns a server-assigned message id (unique within the topic)
+            ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+        }finally {
+            if (publisher != null) {
+                // When finished with the publisher, shutdown to free up resources.
+                publisher.shutdown();
+                //publisher.awaitTermination(1, TimeUnit.MINUTES);
+            }
+        }
+
+
+
+        //Booking b =new Booking(UUID.randomUUID(),LocalDateTime.now(),tickets,customer);
+        //bookings.add(b);
     }
 }
