b57b3f08fe0e2a1a83567bd96cdd07441044c155
Sobhangh <47889077+Sobhangh@users.noreply.github.com>
2024-06-25 16:35:01 +0200

    Update Application.java

--- src/main/java/be/kuleuven/distributedsystems/cloud/Application.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/Application.java
@@ -1,5 +1,17 @@
 package be.kuleuven.distributedsystems.cloud;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GrpcTransportChannel;
+import com.google.api.gax.rpc.FixedTransportChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
+import com.google.cloud.pubsub.v1.*;
+import com.google.pubsub.v1.ProjectSubscriptionName;
+import com.google.pubsub.v1.PushConfig;
+import com.google.pubsub.v1.Subscription;
+import com.google.pubsub.v1.TopicName;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
@@ -16,7 +28,9 @@ import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
+import javax.annotation.Resource;
 import javax.net.ssl.SSLException;
+import java.io.IOException;
 import java.util.Objects;
 
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
@@ -24,7 +38,7 @@ import java.util.Objects;
 public class Application {
 
     @SuppressWarnings("unchecked")
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.setProperty("server.port", System.getenv().getOrDefault("PORT", "8080"));
 
         // Apache JSP scans by default all JARs, which is not necessary, so disable it
@@ -33,9 +47,42 @@ public class Application {
 
         // Start Spring Boot application
         ApplicationContext context = SpringApplication.run(Application.class, args);
+        String subscriptionId = "reserveTicket";
+        //make this as a bean
+        String topicId = "Booking";
+        String pushEndpoint = "https://localhost/8080";
+
+        createPushSubscriptionExample("demo-distributed-systems-kul", subscriptionId, topicId, pushEndpoint);
     }
 
-    @Bean
+    public static void createPushSubscriptionExample(
+            String projectId, String subscriptionId, String topicId, String pushEndpoint)
+            throws IOException {
+        String hostport = System.getenv("PUBSUB_EMULATOR_HOST");
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(String.valueOf(8083)).usePlaintext().build();
+        TransportChannelProvider channelProvider =
+                FixedTransportChannelProvider.create(GrpcTransportChannel.create(channel));
+        CredentialsProvider credentialsProvider = NoCredentialsProvider.create();
+        try (SubscriptionAdminClient subscriptionAdminClient = SubscriptionAdminClient.create(SubscriptionAdminSettings.newBuilder()
+                .setTransportChannelProvider(channelProvider)
+                .setCredentialsProvider(credentialsProvider)
+                .build())) {
+
+            TopicName topicName = TopicName.of(projectId, topicId);
+            ProjectSubscriptionName subscriptionName =
+                    ProjectSubscriptionName.of(projectId, subscriptionId);
+            PushConfig pushConfig = PushConfig.newBuilder().setPushEndpoint(pushEndpoint).build();
+
+            // Create a push subscription with default acknowledgement deadline of 10 seconds.
+            // Messages not successfully acknowledged within 10 seconds will get resent by the server.
+
+            Subscription subscription =
+                    subscriptionAdminClient.createSubscription(subscriptionName, topicName, pushConfig, 60);
+            System.out.println("Created push subscription: " + subscription.getName());
+        }
+    }
+        @Bean
     public boolean isProduction() {
         return Objects.equals(System.getenv("GAE_ENV"), "standard");
     }
